d2cd93193894f0351d4c224a5df68fceac898b76
Rashee <rasheetg165@gmail.com>
2023-08-07 23:21:26 +0530

    I made some changes in login and signup pages

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-document.getElementById("loginForm").addEventListener("submit", function (e) {
+/*document.getElementById("loginForm").addEventListener("submit", function (e) {
   e.preventDefault();
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
@@ -11,4 +11,25 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
   } else {
     alert("Invalid credentials. Please try again.");
   }
+});*/
+
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  // Retrieve user information from local storage
+  const users = JSON.parse(localStorage.getItem("users")) || [];
+  const user = users.find(
+    (u) => u.username === username && u.password === password
+  );
+
+  if (user) {
+    alert("Login successful!");
+    // Redirect to the dashboard or homepage after successful login
+    window.location.href = "index.html";
+  } else {
+    alert("Invalid credentials. Please try again.");
+  }
 });
+
--- signup.js
+++ signup.js
@@ -1,4 +1,4 @@
-document.getElementById("signupForm").addEventListener("submit", function (e) {
+/*document.getElementById("signupForm").addEventListener("submit", function (e) {
   e.preventDefault();
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
@@ -8,4 +8,30 @@ document.getElementById("signupForm").addEventListener("submit", function (e) {
   alert("Account created successfully! You can now login.");
   // Redirect to the login page after successful sign-up
   window.location.href = "login.html";
+});*/
+
+document.getElementById("signupForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  // Retrieve existing users from local storage or initialize an empty array
+  const users = JSON.parse(localStorage.getItem("users")) || [];
+
+  // Check if the username is already taken
+  const isUsernameTaken = users.some((user) => user.username === username);
+  if (isUsernameTaken) {
+    alert("Username is already taken. Please choose a different one.");
+  } else {
+    // Add the new user to the array
+    users.push({ username, password });
+
+    // Store the updated array back in local storage
+    localStorage.setItem("users", JSON.stringify(users));
+
+    alert("Account created successfully! You can now login.");
+    // Redirect to the login page after successful sign-up
+    window.location.href = "login.html";
+  }
 });
+
