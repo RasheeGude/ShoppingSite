77bc08fe82432d58155190c548bb27463db037d4
Rashee <rasheetg165@gmail.com>
2023-08-07 23:09:33 +0530

    I changed the login form.

--- login.js
+++ login.js
@@ -7,7 +7,7 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
   if (username === "example" && password === "password") {
     alert("Login successful!");
     // Redirect to the dashboard or homepage after successful login
-    window.location.href = "dashboard.html";
+    window.location.href = "index.html";
   } else {
     alert("Invalid credentials. Please try again.");
   }
